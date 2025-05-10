bc6efe1b3c1cc6cf6de0595643e989b5a80aaf0b
sjaggi1234567890 <140255542+sjaggi1234567890@users.noreply.github.com>
2025-05-11 00:27:25 +0530

    Update defaultConstructor.cpp
    Added 2 extra comments

--- OOPS/Constructor/defaultConstructor.cpp
+++ OOPS/Constructor/defaultConstructor.cpp
@@ -8,7 +8,7 @@ private:
 
 public:
     // Default constructor
-    MyClass() {
+    MyClass() {                                    // it is mandatory that a constructor has the same name as the class 
         x = 0; // Initialize x to 0
         cout << "Default constructor called. x = " << x << endl;
     }
@@ -21,7 +21,7 @@ public:
 
 int main() {
     // Creating an object using the default constructor
-    MyClass obj1;
+    MyClass obj1;                                     //constrcutor is automatically called as soon as obj1 is created                
     obj1.display();
     return 0;
 }
